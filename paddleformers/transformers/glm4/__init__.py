34fc9d96a3b9e1c2363a49284c0533b46856887e
ssstuding <2687911365@qq.com>
2026-04-02 20:01:50 +0800

    添加GLM4到paddleformers

---
+++ paddleformers/transformers/glm4/__init__.py
@@ -0,0 +1,9 @@
+from .configuration import Glm4Config
+from .modeling import Glm4PretrainedModel, Glm4Model, Glm4ForCausalLM
+
+__all__ = [
+    "Glm4Config",
+    "Glm4PretrainedModel",
+    "Glm4Model",
+    "Glm4ForCausalLM",
+]
